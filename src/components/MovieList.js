ca18c0789496a8c0740cf79ec5efeba506022a1b
Brian Nunez <nunez27@brians-mbp.myfiosgateway.com>
2021-06-08 18:44:24 -0400

    Added movielist to display on screen

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
 
-const MovieList = (props)=> {
-    const movies = [];
+const MovieList = ({movies})=> {
 
     return (
         <div className="col">
@@ -31,4 +30,10 @@ const MovieList = (props)=> {
     );
 }
 
-export default MovieList;
+const mapStateToProps = (state) => {
+    return {
+        movies: state.movies
+    };
+}
+
+export default connect(mapStateToProps)(MovieList);
